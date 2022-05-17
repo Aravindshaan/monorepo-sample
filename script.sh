558a2941fc6f48fd1300eeccf749c4813f5b54cc
Aravindshaan <90775234+Aravindshaan@users.noreply.github.com>
2022-05-16 21:15:18 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /home
-cd /home; unzip -uo /home/runner/work/monorepo-sample/monorepo-sample/apps/app2/target/app2-1.0-SNAPSHOT.jar
-cd /home; unzip -uo /home/runner/work/monorepo-sample/monorepo-sample/apps/app2/target/app2-1.0-SNAPSHOT.jar
-jar -cvf combined.jar -C home 
+unzip -uo /home/runner/work/monorepo-sample/monorepo-sample/apps/app2/target/app2-1.0-SNAPSHOT.jar
+unzip -uo /home/runner/work/monorepo-sample/monorepo-sample/apps/app2/target/app2-1.0-SNAPSHOT.jar
+jar -cvf combined.jar -C . 
